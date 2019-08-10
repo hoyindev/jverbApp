4592053cac0c2782f0b821d97fa0936aacbeb4eb
hoyin <hoyindev@gmail.com>
2019-08-10 17:59:59 +0930

    set up the structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './home/about/about.component';
+import { StudyComponent } from './home/study/study.component';
+import { TestComponent } from './home/test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    StudyComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
